bd7b61671525b27ac444e32d75a9f7fd72c9493d
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-04-23 16:24:05 -0700

    created renderDepartmentsArray() function that loops through employees object and creates an array of all departments, in the file filter.js

--- react-directory/src/components/filter.js
+++ react-directory/src/components/filter.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import employeeList from './employeeList';
 import Table from './table';
+let departments=[];
+
+//puts all unique departments into an array
+function renderDepartmentsArray() {
+    for(let i=0; i<employeeList.length; i++) {
+        if(departments.indexOf(employeeList[i].department) == -1) {
+            departments.push(employeeList[i].department)
+        }
+    }
+    console.log(departments)
+}
 
 function Filter(department) {
+
     let list = employeeList.filter(e => e.department.includes('HR'))
     return <Table employees={list}></Table>
 }
