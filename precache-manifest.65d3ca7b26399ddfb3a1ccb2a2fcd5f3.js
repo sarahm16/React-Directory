618da00d2fe2bfcd8487ad91a08eaaae4cb4c931
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-04-28 11:43:07 -0700

    Updates

---
+++ precache-manifest.65d3ca7b26399ddfb3a1ccb2a2fcd5f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf60cb89606bbee18fc5fd33999d1f04",
+    "url": "/react-directory/index.html"
+  },
+  {
+    "revision": "436cc7b8c983faa7a1dd",
+    "url": "/react-directory/static/css/main.0a450555.chunk.css"
+  },
+  {
+    "revision": "1c3aa08c0d7d2315aed0",
+    "url": "/react-directory/static/js/2.d052b199.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-directory/static/js/2.d052b199.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "436cc7b8c983faa7a1dd",
+    "url": "/react-directory/static/js/main.9c0a317c.chunk.js"
+  },
+  {
+    "revision": "7e63d402c833a61e1f0b",
+    "url": "/react-directory/static/js/runtime-main.a11737cd.js"
+  },
+  {
+    "revision": "a102e7a369aeaf48d6e8f02b7f9465b2",
+    "url": "/react-directory/static/media/waves.a102e7a3.jpg"
+  }
+]);
